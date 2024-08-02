c3bd95226e4e770cdae198dbfbbe2c40f2547730
Joanna <joanna_pinczuk@hotmail.com>
2024-08-02 12:39:33 +0200

    update

--- src/store.js
+++ src/store.js
@@ -1,31 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import authReducer from "./slices/authSlice";
-import contactsReducer from "./slices/contactsSlice";
-import filterReducer from "./slices/filterSlice";
-
-const persistConfig = {
-    key: 'root',
-    storage,
-    whitelist: ['auth'],
-  };
-
-const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+import { configureStore } from '@reduxjs/toolkit';
+import contactsReducer from './slices/contactsSlice';
+import filterReducer from './slices/filterSlice';
 
 const store = configureStore({
-    reducer: {
-      auth: persistedAuthReducer,
-      contacts: contactsReducer,
-      filter: filterReducer,
-    },
-    middleware: (getDefaultMiddleware) => 
-      getDefaultMiddleware({
-        serializableCheck: {
-          ignoredActions: [persistStore.register],
-        },
-  }),
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
 });
-const persistor = persistStore(store);
-
-export { store, persistor };
+export default store;
