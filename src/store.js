0d242e9a9b897c5780b17678f3390a1fc3ad8177
Joanna <joanna_pinczuk@hotmail.com>
2024-08-02 13:07:59 +0200

    updates

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './slices/contactsSlice';
 import filterReducer from './slices/filterSlice';
+import authReducer from './slices/authSlice';
 
 const store = configureStore({
   reducer: {
+    auth: authReducer,
     contacts: contactsReducer,
     filter: filterReducer,
   },
