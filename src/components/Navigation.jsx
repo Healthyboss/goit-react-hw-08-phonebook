a4befa057439a058bcf6ed67264f787699657f89
Joanna <joanna_pinczuk@hotmail.com>
2024-08-06 18:57:43 +0200

    updates

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import UserMenu from './UserMenu';
+import styles from "./Navigation.module.css";
 
 const Navigation = () => {
     const { user } = useSelector((state) => state.auth);
   
     return (
-      <nav>
+      <nav className={styles.navigation}>
         <Link to="/contacts">Contacts</Link>
         {user ? (
           <UserMenu />
--- src/store.js
+++ src/store.js
@@ -1,24 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import { 
+  persistStore, 
+  persistReducer,
+  FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER,
+ } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import contactsReducer from './slices/contactsSlice';
 import filterReducer from './slices/filterSlice';
 import authReducer from './slices/authSlice';
 
 const persistConfig = {
-  key: 'root',
+  key: "auth",
   storage,
-  whitelist: ['auth'],
+  whitelist: ["token"],
 };
-
-const persistedAuthReducer = persistReducer(persistConfig, authReducer);
-
 const store = configureStore({
   reducer: {
-    auth: persistedAuthReducer,
+    auth: persistReducer( persistConfig, authReducer),
     contacts: contactsReducer,
     filter: filterReducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 const persistor = persistStore(store);
